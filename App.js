6b9cbdffe6b58002344dea9a387d4286384b0f1a
Bhagwat Karankar <dev.pacific22@gmail.com>
2022-03-31 23:47:03 +0530

    Add: Added native base and vector-icons.

--- App.js
+++ App.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, StyleSheet, Platform, StatusBar} from 'react-native';
 import AppContainer from './src/routes/Routes';
 import {Provider} from 'react-redux';
 import getStore from './src/redux';
+import {Colors} from './src/utils/Colors';
+import {NativeBaseProvider} from 'native-base';
 
 const store = getStore();
 const App = () => {
   return (
     <Provider store={store}>
-      <AppContainer />
+      <NativeBaseProvider>
+        <SafeAreaView style={[styles.container]}>
+          <AppContainer />
+        </SafeAreaView>
+      </NativeBaseProvider>
     </Provider>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.pageBackground,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+  },
+});
 
 export default App;
